fd7bb3fc2884b1ba60a1447d7a77a7e592abe2fc
Admin <phamdaomy123456@gmail.com>
2025-08-15 18:43:30 +0700

    first

--- Mytra/Tramy.py
+++ Mytra/Tramy.py
@@ -1,2 +1,7 @@
 full_name = input("Nhập tên của bạn vào: ")
 print(f"Welcome {full_name} đến với ngôi nhà của chúng tôi")
+x= str(50)
+y= x+ "Phạm My "
+print(y)
+txt = "We are the so-called \"Vikings \" from the north."
+print(txt)
